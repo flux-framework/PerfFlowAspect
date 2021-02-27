099206959f3d9d6d9b5d44b5dd0094d9e9f706f4
Dong H. Ahn <ahn1@llnl.gov>
2021-02-27 02:48:41 -0800

    weavepass: LLVM pass to detect annotations and weave
    Unlike dynamic programming languages such as Python,
    C/C++ cannot natively and easily weave PerfFlowAspect
    into the target program (e.g., using language
    features like Python decorator).
    To overcome this language-level limitation,
    PerfFlowAspect introduces a simple
    LLVM pass which can detect critical-path annotations
    in the target C/C++ source files and to weave
    these annotated program points with
    PerfFlowAspect's runtime library as part
    of Clang compilers' optimization pass.
    
    Extend the FunctionPass class of LLVM Pass framework.
    
    Detect annotations embedded in compiler directives,
    for example,
    __attribute__((annotate("@critical_path()")))
    
    Insert PerfFlowAspect runtime function calls
    on each and every annotated function as part
    of this pass.
    
    When pointcut="around" (default), weave
    for both the entering and exiting
    of the annotated function.
    When the pointcut is
    either before or after (or their
    async version), weave at in either
    corresponding side of the target
    function execution.

---
+++ src/c/weaver/weave/perfflow_weave.cpp
@@ -0,0 +1,174 @@
+/************************************************************\
+ * Copyright 2021 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#include "llvm/IR/Value.h"
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/CallSite.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Argument.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "../../parser/perfflow_parser.hpp"
+#include "perfflow_weave.hpp"
+
+using namespace llvm;
+
+
+/******************************************************************************
+ *                                                                            *
+ *                 Private Methods of WeavingPass Class                       *
+ *                                                                            *
+ ******************************************************************************/
+
+bool WeavingPass::insertAfter (Module &m, Function &f, StringRef &a,
+                               int async, std::string &scope)
+{
+    if (m.empty () || f.empty ())
+        return false;
+
+    auto &context = m.getContext();
+    Type *voidType = Type::getVoidTy (context);
+    Type *int32Type = Type::getInt32Ty (context);
+    Type *int8PtrType = Type::getInt8PtrTy (context);
+    std::vector<llvm::Type *> params;
+    params.push_back (int32Type);
+    params.push_back (int8PtrType);
+    params.push_back (int8PtrType);
+    params.push_back (int8PtrType);
+
+    // voidType is return type, parms are parameters and no variable length args
+    FunctionType *weaveFuncTy = FunctionType::get (voidType, params, false);
+    // Note: Use FunctionCallee after for Clang 9 or higher
+    // FunctionCallee after = m.getOrInsertFunction("perfflow_weave_after",
+    Constant *after = m.getOrInsertFunction("perfflow_weave_after",
+                                            weaveFuncTy);
+
+    // iterate through blocks
+    for (BasicBlock &bb : f) {
+        Instruction *inst = bb.getTerminator ();
+        if (isa<ReturnInst>(inst) || isa<ResumeInst>(inst)) {
+            IRBuilder<> builder (inst);
+            Value *v1 = builder.CreateGlobalStringPtr (m.getName (), "str");
+            Value *v2 = builder.CreateGlobalStringPtr (f.getName (), "str");
+            Value *v3 = builder.CreateGlobalStringPtr (StringRef (scope), "str");
+            std::vector<Value *> args;
+            args.push_back (ConstantInt::get(Type::getInt32Ty (context), async));
+            args.push_back (v1);
+            args.push_back (v2);
+            args.push_back (v3);
+            builder.CreateCall (after, args);
+        }
+    }
+    return true;
+}
+
+bool WeavingPass::insertBefore (Module &m, Function &f, StringRef &a,
+                                int async, std::string &scope)
+{
+    if (m.empty () || f.empty ())
+        return false;
+
+    auto &context = m.getContext();
+    Type *voidType = Type::getVoidTy (context);
+    Type *int32Type = Type::getInt32Ty (context);
+    Type *int8PtrType = Type::getInt8PtrTy (context);
+    std::vector<llvm::Type *> params;
+    params.push_back (int32Type);
+    params.push_back (int8PtrType);
+    params.push_back (int8PtrType);
+    params.push_back (int8PtrType);
+
+    // voidType is return type, params are parameters and no variable length args
+    FunctionType *weaveFuncTy = FunctionType::get (voidType, params, false);
+    // Note: User FunctionCallee before for Clang >= 9.0  
+    // FunctionCallee before = m.getOrInsertFunction ("perfflow_weave_before",
+    Constant *before = m.getOrInsertFunction ("perfflow_weave_before",
+                                              weaveFuncTy);
+    auto &entry = f.getEntryBlock ();
+    IRBuilder<> builder (&entry);
+    Value *v1 = builder.CreateGlobalStringPtr (m.getName (), "str");
+    Value *v2 = builder.CreateGlobalStringPtr (f.getName (), "str");
+    Value *v3 = builder.CreateGlobalStringPtr (StringRef (scope), "str");
+    builder.SetInsertPoint (&entry, entry.begin ());
+    std::vector<Value *> args;
+    args.push_back (ConstantInt::get(Type::getInt32Ty (context), async));
+    args.push_back (v1);
+    args.push_back (v2);
+    args.push_back (v3);
+    builder.CreateCall (before, args);
+
+    return true;
+}
+
+
+/******************************************************************************
+ *                                                                            *
+ *                 Public Methods of WeavingPass Class                        *
+ *                                                                            *
+ ******************************************************************************/
+
+bool WeavingPass::doInitialization (Module &m)
+{
+    auto annotations = m.getNamedGlobal("llvm.global.annotations");
+    if (!annotations)
+          return false;
+
+    bool changed = false;
+    auto a = cast<ConstantArray> (annotations->getOperand (0));
+    for (unsigned int i = 0; i < a->getNumOperands (); i++) {
+      auto e = cast<ConstantStruct> (a->getOperand (i));
+      if (auto *fn = dyn_cast<Function> (e->getOperand(0)->getOperand (0))) {
+          auto anno = cast<ConstantDataArray>(
+                          cast<GlobalVariable>(e->getOperand(1)->getOperand(0))
+                              ->getOperand(0))
+                          ->getAsCString();
+	  std::string pcut, scope;
+          if (perfflow_parser_parse (anno.data (), pcut, scope) == 0) {
+	      if (pcut == "around" || pcut == "before")
+                  changed = insertBefore (m, *fn, anno, 0, scope) || changed;
+              else if (pcut == "around_async" || pcut == "before_async")
+                  changed = insertBefore (m, *fn, anno, 1, scope) || changed;
+	      if (pcut == "around" || pcut == "after")
+                  changed = insertAfter (m, *fn, anno, 0, scope) || changed;
+              else if (pcut == "around_async" || pcut == "after_async")
+                  changed = insertBefore (m, *fn, anno, 1, scope) || changed;
+          } else {
+              errs () << "WeavePass[WARN]: Ignoring " << anno << "\n";
+          }
+      }
+    }
+    return changed;
+}
+
+bool WeavingPass::runOnFunction (Function &F)
+{
+    return false;
+}
+
+char WeavingPass::ID = 0;
+
+// Automatically enable the pass.
+// http://adriansampson.net/blog/clangpass.html
+static void registerWeavingPass(const PassManagerBuilder &,
+                         legacy::PassManagerBase &PM) {
+  PM.add(new WeavingPass());
+}
+
+static RegisterStandardPasses
+  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+                 registerWeavingPass);
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
---
+++ src/c/weaver/weave/perfflow_weave.hpp
@@ -0,0 +1,46 @@
+/************************************************************\
+ * Copyright 2021 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#ifndef PERFFLOW_WEAVE_H
+#define PERFFLOW_WEAVE_H
+
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+
+#include <string>
+
+using namespace llvm;
+
+namespace {
+
+class WeavingPass : public FunctionPass
+{
+public:
+    static char ID;
+    WeavingPass () : FunctionPass (ID) {}
+    virtual bool doInitialization (Module &m);
+    virtual bool runOnFunction (Function &F);
+
+private:
+    bool insertAfter (Module &m, Function &f, StringRef &a,
+                      int async, std::string &scope);
+    bool insertBefore (Module &m, Function &f, StringRef &a,
+                       int async, std::string &scope);
+};
+
+}
+
+#endif // PERFFLOW_WEAVE_H
+
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
