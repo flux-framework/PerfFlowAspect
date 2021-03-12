acdc26e35ac7a305c6efa3b182a364b30e240116
Stephen Herbein <stephen272@gmail.com>
2021-03-12 14:09:14 -0800

    aspect: add APIs for directly logging events
    Problem: For Maestro and GMD, it is sometimes more ergonomoic to insert
    function calls that directly log events as opposed to using the decorators.
    
    Solution: add two methods (create_[a]sync_event) that enable direct logging of
    events.
    
    Closes #16

---
+++ src/python/tests/smoketest_direct.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+import time
+import os.path
+from perfflowaspect import aspect
+
+
+def foo():
+    aspect.sync_event("before", "foo", filename)
+    time.sleep(2)
+    print("hello")
+    aspect.sync_event("after", "foo", filename)
+
+
+def main():
+    aspect.sync_event("before", "main", filename)
+    foo()
+    aspect.sync_event("after", "main", filename)
+
+
+if __name__ == "__main__":
+    filename = os.path.basename(__file__)
+    main()
