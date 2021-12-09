85b87eb9a60cd2181e80d4f03563edc61aee8d4e
Stephanie Brink <brink2@llnl.gov>
2021-12-09 12:21:46 -0800

    add empty decorator, logic for enable/disable PerfFlow
    Adds an empty decorator that runs when logging is disabled, else run specified
    advice.

--- src/python/perfflowaspect/advice_dispatch.py
+++ src/python/perfflowaspect/advice_dispatch.py
@@ -8,7 +8,16 @@
 # SPDX-License-Identifier: LGPL-3.0
 ##############################################################
 
+import sys
 from .advice_chrome import ChromeTracingAdvice
+
+
+def _disable(func):
+    "Do nothing decorator (e.g., disables func)"
+    def do_nothing_func(*args, **kargs):
+        print("perfflow warning: logging has been disabled for {}".format(func.__name__), file=sys.stderr)
+        pass
+    return do_nothing_func
 
 
 class AdviceDispatcher:
@@ -20,17 +29,17 @@ class AdviceDispatcher:
 
     def _dispatch(clobj, pointcut, scope):
         if pointcut == "around":
-            return clobj.around
+            return clobj.around if ChromeTracingAdvice.enable_logging else _disable
         elif pointcut == "before":
-            return clobj.before
+            return clobj.before if ChromeTracingAdvice.enable_logging else _disable
         elif pointcut == "after":
-            return clobj.after
+            return clobj.after if ChromeTracingAdvice.enable_logging else _disable
         elif pointcut == "around_async":
-            return clobj.around_async
+            return clobj.around_async if ChromeTracingAdvice.enable_logging else _disable
         elif pointcut == "before_async":
-            return clobj.before_async(scope)
+            return clobj.before_async(scope) if ChromeTracingAdvice.enable_logging else _disable
         elif pointcut == "after_async":
-            return clobj.after_async(scope)
+            return clobj.after_async(scope) if ChromeTracingAdvice.enable_logging else _disable
         raise KeyError("unknown pointcut", pointcut)
 
     def get(key, pointcut, scope):
