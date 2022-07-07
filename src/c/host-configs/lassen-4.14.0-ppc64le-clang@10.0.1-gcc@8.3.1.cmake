979deeb9e9d24942398fd59a224f219a8041d43e
Stephanie Brink <brink2@llnl.gov>
2022-07-06 20:14:37 -0700

    add host config file for lassen

---
+++ src/c/host-configs/lassen-4.14.0-ppc64le-clang@10.0.1-gcc@8.3.1.cmake
@@ -0,0 +1,6 @@
+# Copyright 2019-2021 Lawrence Livermore National Security, LLC and other
+# Variorum Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-10.0.1-gcc-8.3.1/bin/clang++" CACHE PATH "")
