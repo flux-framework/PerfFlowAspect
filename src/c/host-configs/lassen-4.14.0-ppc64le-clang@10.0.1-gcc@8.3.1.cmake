970df78480233bbdd4a281e974967cedd9d808dd
Stephanie Brink <brink2@llnl.gov>
2022-07-11 22:04:02 -0700

    update license header in host config file

--- src/c/host-configs/lassen-4.14.0-ppc64le-clang@10.0.1-gcc@8.3.1.cmake
+++ src/c/host-configs/lassen-4.14.0-ppc64le-clang@10.0.1-gcc@8.3.1.cmake
@@ -1,6 +1,11 @@
-# Copyright 2019-2021 Lawrence Livermore National Security, LLC and other
-# Variorum Project Developers. See the top-level LICENSE file for details.
+##############################################################
+# Copyright 2021 Lawrence Livermore National Security, LLC
+# (c.f. AUTHORS, NOTICE.LLNS, COPYING)
 #
-# SPDX-License-Identifier: MIT
+# This file is part of the Flux resource manager framework.
+# For details, see https://github.com/flux-framework.
+#
+# SPDX-License-Identifier: LGPL-3.0
+##############################################################
 
 set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-10.0.1-gcc-8.3.1/bin/clang++" CACHE PATH "")
