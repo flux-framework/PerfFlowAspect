a1cf580f1317cbdbe972ee1063330beca897fd15
Aliza Lisan <36656209+alizalisan@users.noreply.github.com>
2023-03-20 14:32:34 -0700

    Cuda smoketest in C (#87)
    * add cuda smoketest files, CMakeLists changes and tets cases
    
    * add nvidia-cuda-toolkit to CI environment, check nvcc version
    
    * install smoketest_cuda test binary
    
    * add conditional for cuda smoketests
    
    only run cuda smoketest on a platform where NVIDIA GPU exists
    
    Co-authored-by: Stephanie Brink <brink2@llnl.gov>

---
+++ src/c/test/smoketest_cuda_wrapper.cpp
@@ -0,0 +1,16 @@
+/************************************************************\
+ * Copyright 2021 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#include "smoketest_cuda_kernel.cuh"
+
+int main(int argc, char *argv[])
+{
+    Wrapper::wrapper();
+}
