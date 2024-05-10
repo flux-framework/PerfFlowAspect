749f7f35d0bdabc3ed5ceaafb9f3aa4be0cecda2
Stephanie Brink <brink2@llnl.gov>
2024-05-10 10:09:19 -0700

    toggle caliper build on/off

--- src/c/cmake/Setup3rdParty.cmake
+++ src/c/cmake/Setup3rdParty.cmake
@@ -20,12 +20,14 @@ if(PERFFLOWASPECT_WITH_MULTITHREADS)
     include(cmake/thirdparty/FindThreads.cmake)
 endif()
 
-# first Check for CALIPER_DIR
-if(NOT caliper_DIR)
-    MESSAGE(FATAL_ERROR "Caliper support needs explicit caliper_DIR")
-endif()
+if(PERFFLOWASPECT_WITH_CALIPER)
+    # first Check for CALIPER_DIR
+    if(NOT caliper_DIR)
+        MESSAGE(FATAL_ERROR "Caliper support needs explicit caliper_DIR")
+    endif()
 
-if(caliper_DIR)
-    message(STATUS "PPP ${caliper_DIR}")
-    include(cmake/thirdparty/SetupCaliper.cmake)
+    if(caliper_DIR)
+        message(STATUS "PPP ${caliper_DIR}")
+        include(cmake/thirdparty/FindCaliper.cmake)
+    endif()
 endif()
