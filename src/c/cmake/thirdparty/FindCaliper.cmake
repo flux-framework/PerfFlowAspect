e9f761063c1fb65f02a8726374ac5b943c0cdb30
Stephanie Brink <brink2@llnl.gov>
2024-05-10 11:11:08 -0700

    refine FindCaliper to ignore global caliper installs

--- src/c/cmake/thirdparty/FindCaliper.cmake
+++ src/c/cmake/thirdparty/FindCaliper.cmake
@@ -17,7 +17,12 @@
 message(STATUS "Looking for Caliper in: ${caliper_DIR}")
 
 find_package(caliper REQUIRED
-             PATHS ${caliper_DIR}/share/cmake/caliper)
+             PATHS ${caliper_DIR}/share/cmake/caliper
+             NO_DEFAULT_PATH
+             NO_CMAKE_ENVIRONMENT_PATH
+             NO_CMAKE_PATH
+             NO_SYSTEM_ENVIRONMENT_PATH
+             NO_CMAKE_SYSTEM_PATH)
 
 message(STATUS "FOUND Caliper: ${caliper_INSTALL_PREFIX}")
 
