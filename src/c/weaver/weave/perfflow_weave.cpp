18e5b0a85ca8bd1c6f4e9394ebab2f96c429e2ed
Aliza Lisan <lisan1@lassen33.coral.llnl.gov>
2024-07-30 16:16:53 -0700

    Initial commit for Adiak integration, cmake changes, and smoketest_adiak

--- src/c/weaver/weave/perfflow_weave.cpp
+++ src/c/weaver/weave/perfflow_weave.cpp
@@ -26,6 +26,12 @@
 #include "../../parser/perfflow_parser.hpp"
 #include "perfflow_weave.hpp"
 
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+#include <adiak_tool.h>
+#define UNUSED(x) (void)(x)
+
 using namespace llvm;
 
 
@@ -142,8 +148,135 @@ bool WeavingPass::insertBefore(Module &m, Function &f, StringRef &a,
  *                                                                            *
  ******************************************************************************/
 
+// static void print_value(adiak_value_t *val, adiak_datatype_t *t)
+// {
+//    if (!t)
+//       printf("ERROR");
+//    switch (t->dtype) {
+//       case adiak_type_unset:
+//          printf("UNSET");
+//          break;
+//       case adiak_long:
+//          printf("%ld", val->v_long);
+//          break;
+//       case adiak_ulong:
+//          printf("%lu", (unsigned long) val->v_long);
+//          break;
+//       case adiak_longlong:
+//          printf("%lld", val->v_longlong);
+//          break;
+//       case adiak_ulonglong:
+//          printf("%llu", (unsigned long long) val->v_longlong);
+//          break;
+//       case adiak_int:
+//          printf("%d", val->v_int);
+//          break;
+//       case adiak_uint:
+//          printf("%u", (unsigned int) val->v_int);
+//          break;
+//       case adiak_double:
+//          printf("%f", val->v_double);
+//          break;
+//       case adiak_date: {
+//          char datestr[512];
+//          signed long seconds_since_epoch = (signed long) val->v_long;
+//          struct tm *loc = localtime(&seconds_since_epoch);
+//          strftime(datestr, sizeof(datestr), "%a, %d %b %Y %T %z", loc);
+//          printf("%s", datestr);
+//          break;
+//       }
+//       case adiak_timeval: {
+//          struct timeval *tval = (struct timeval *) val->v_ptr;
+//          double duration = tval->tv_sec + (tval->tv_usec / 1000000.0);
+//          printf("%fs (timeval)", duration);
+//          break;
+//       }
+//       case adiak_version: {
+//          char *s = (char *) val->v_ptr;
+//          printf("%s (version)", s);
+//          break;
+//       }
+//       case adiak_string: {
+//          char *s = (char *) val->v_ptr;
+//          printf("%s (string)", s);
+//          break;
+//       }
+//       case adiak_catstring: {
+//          char *s = (char *) val->v_ptr;
+//          printf("%s (catstring)", s);
+//          break;
+//       }
+//       case adiak_path: {
+//          char *s = (char *) val->v_ptr;
+//          printf("%s (path)", s);
+//          break;
+//       }
+//       case adiak_range: {
+//          adiak_value_t subvals[2];
+//          adiak_datatype_t* subtypes[2];
+
+//          adiak_get_subval(t, val, 0, subtypes+0, subvals+0);
+//          adiak_get_subval(t, val, 1, subtypes+1, subvals+1);
+
+//          print_value(subvals+0, *(subtypes+0));
+//          printf(" - ");
+//          print_value(subvals+1, *(subtypes+1));
+//          break;
+//       }
+//       case adiak_set: {
+//          printf("[");
+//          for (int i = 0; i < adiak_num_subvals(t); i++) {
+//             adiak_value_t subval;
+//             adiak_datatype_t* subtype;
+//             adiak_get_subval(t, val, i, &subtype, &subval);
+//             print_value(&subval, subtype);
+//             if (i+1 != t->num_elements)
+//                printf(", ");
+//          }
+//          printf("]");
+//          break;
+//       }
+//       case adiak_list: {
+//          printf("{");
+//          for (int i = 0; i < adiak_num_subvals(t); i++) {
+//             adiak_value_t subval;
+//             adiak_datatype_t* subtype;
+//             adiak_get_subval(t, val, i, &subtype, &subval);
+//             print_value(&subval, subtype);
+//             if (i+1 != t->num_elements)
+//                printf(", ");
+//          }
+//          printf("}");
+//          break;
+//       }
+//       case adiak_tuple: {
+//          printf("(");
+//          for (int i = 0; i < adiak_num_subvals(t); i++) {
+//             adiak_value_t subval;
+//             adiak_datatype_t* subtype;
+//             adiak_get_subval(t, val, i, &subtype, &subval);
+//             print_value(&subval, subtype);
+//             if (i+1 != t->num_elements)
+//                printf(", ");
+//          }
+//          printf(")");
+//          break;
+//       }
+//    }
+// }
+
+static void print_nameval(const char *name, int category, const char *subcategory, adiak_value_t *value, adiak_datatype_t *t, void *opaque_value)
+{
+    // printf("%s: ", name);
+    printf("%s: ", "In the callback print");
+    outs() << "In the callback \n";
+    // print_value(value, t);
+    // printf("\n");
+}
+
 bool WeavingPass::doInitialization(Module &m)
 {
+    adiak_register_cb(1, adiak_category_all, print_nameval, 0, NULL);
     outs() << "WeavePass loaded successfully. \n";
 
     auto annotations = m.getNamedGlobal("llvm.global.annotations");
