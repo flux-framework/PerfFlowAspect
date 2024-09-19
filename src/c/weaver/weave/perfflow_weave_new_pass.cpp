83f20f6c1462edbc489a776e5dc757f7b5d236a3
Tapasya Patki <patki1@llnl.gov>
2024-09-18 23:46:25 -0700

    Add a basic module pass structure for perfflow_weave_new_pass. Untested.

---
+++ src/c/weaver/weave/perfflow_weave_new_pass.cpp
@@ -0,0 +1,107 @@
+/************************************************************\
+ * Copyright 2021 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#include "llvm/IR/Value.h"
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/AbstractCallSite.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Argument.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "../../parser/perfflow_parser.hpp"
+#include "perfflow_weave_new_pass.hpp"
+
+using namespace llvm;
+
+// Implement the runOnModule Function
+bool NewWeavingPass::runOnModule(Module &M)
+{
+    bool PatkiTest = true;
+    // We do nothing right now, let's build to see if this goes through and loads correctly.
+    // The build will probably fail as we are not parsing the annotations.
+
+    outs() << "NewWeavePass loaded successfully. \n";
+    errs() << "NewWeavePass had some error. \n";
+
+    // The following loops through each function. This is where we need to check annotation 
+    // (from doInitialization in the legacy pass and add the insertBefore/insertAfter)
+    // for (auto &F : M)
+    // {
+    //     if (F.isDeclaration())
+    //         continue;
+
+    //     // Get an IR builder. Sets the insertion point to the top of the function
+    //     IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
+
+    //     // Inject a global variable that contains the function name
+    //     auto FuncName = Builder.CreateGlobalStringPtr(F.getName());
+
+    //     // Printf requires i8*, but PrintfFormatStrVar is an array: [n x i8]. Add
+    //     // a cast: [n x i8] -> i8*
+    //     llvm::Value *FormatStrPtr =
+    //         Builder.CreatePointerCast(PrintfFormatStrVar, PrintfArgTy, "formatStr");
+
+    //     // The following is visible only if you pass -debug on the command line
+    //     // *and* you have an assert build.
+    //     LLVM_DEBUG(dbgs() << " Injecting call to printf inside " << F.getName()
+    //                       << "\n");
+
+    //     // Finally, inject a call to printf
+    //     Builder.CreateCall(
+    //         Printf, {FormatStrPtr, FuncName, Builder.getInt32(F.arg_size())});
+
+    //     InsertedAtLeastOnePrintf = true;
+    // }
+
+    return PatkiTest;
+}
+
+// Run on module function.
+PreservedAnalyses NewWeavingPass::run(llvm::Module &M,
+                                      llvm::ModuleAnalysisManager &)
+{
+    bool Changed = runOnModule(M);
+
+    return (Changed ? llvm::PreservedAnalyses::none()
+                    : llvm::PreservedAnalyses::all());
+}
+
+// Register the new pass.
+// See docs here: https://github.com/banach-space/llvm-tutor/blob/main/lib/InjectFuncCall.cpp#L139
+// And here: https://rocmdocs.amd.com/projects/llvm-project/en/latest/LLVM/llvm/html/WritingAnLLVMNewPMPass.html
+
+llvm::PassPluginLibraryInfo getNewWeavingPassPluginInfo()
+{
+    return {LLVM_PLUGIN_API_VERSION, "inject-func-call", LLVM_VERSION_STRING,
+            [](PassBuilder &PB)
+            {
+                PB.registerPipelineParsingCallback(
+                    [](StringRef Name, ModulePassManager &MPM,
+                       ArrayRef<PassBuilder::PipelineElement>)
+                    {
+                        if (Name == "new-weaving-pass")
+                        {
+                            MPM.addPass(NewWeavingPass());
+                            return true;
+                        }
+                        return false;
+                    });
+            }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo()
+{
+    return getNewWeavingPassPluginInfo();
+}
---
+++ src/c/weaver/weave/perfflow_weave_new_pass.hpp
@@ -0,0 +1,61 @@
+/************************************************************\
+ * Copyright 2021 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#ifndef PERFFLOW_WEAVE_NEW_PASS_H
+#define PERFFLOW_WEAVE_NEW_PASS_H
+
+#include "llvm/IR/PassManager.h"
+#include "llvm/Pass.h"
+
+#include <string>
+
+using namespace llvm;
+
+namespace {
+
+class NewWeavingPass : public PassInfoMixin<NewWeavingPass> {
+public:
+  // Revist this if we really need a function pass 
+  // PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &);
+  bool runOnModule(Module &M);
+
+  // Without isRequired returning true, this pass will be skipped for functions
+  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+  // all functions with optnone.
+  static bool isRequired() { return true; }
+
+};
+
+
+
+// class WeavingPass : public FunctionPass
+// {
+// public:
+//     static char ID;
+//     WeavingPass () : FunctionPass (ID) {}
+//     virtual bool doInitialization (Module &m);
+//     virtual bool runOnFunction (Function &F);
+
+// private:
+//     bool insertAfter (Module &m, Function &f, StringRef &a,
+//                       int async, std::string &scope, std::string &flow, std::string pcut);
+//     bool insertBefore (Module &m, Function &f, StringRef &a,
+//                        int async, std::string &scope, std::string &flow, std::string pcut);
+// };
+
+// }
+
+#endif // PERFFLOW_WEAVE_NEW_PASS_H
+
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
