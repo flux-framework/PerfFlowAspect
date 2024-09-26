18f0e602d8d9e0306e1be413617a9c8dba475938
Stephanie Brink <brink2@llnl.gov>
2024-09-26 12:29:40 -0700

    print operand names

--- src/c/host-configs/lassen-4.14.0-ppc64le-clang@16.0.6-gcc@11.2.1.cmake
+++ src/c/host-configs/lassen-4.14.0-ppc64le-clang@16.0.6-gcc@11.2.1.cmake
@@ -8,6 +8,7 @@
 # SPDX-License-Identifier: LGPL-3.0
 ##############################################################
 
-set(CMAKE_C_COMPILER "/usr/tce/packages/clang/clang-14.0.5-gcc-8.3.1/bin/clang" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-14.0.5-gcc-8.3.1/bin/clang++" CACHE PATH "")
-set(LLVM_DIR "/usr/tce/packages/clang/clang-ibm-14.0.5-gcc-8.3.1/lib/cmake/llvm" CACHE PATH "")
+set(CMAKE_C_COMPILER "/usr/tce/packages/clang/clang-16.0.6-gcc-11.2.1/bin/clang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-16.0.6-gcc-11.2.1/bin/clang++" CACHE PATH "")
+set(LLVM_DIR "/usr/tce/packages/clang/clang-ibm-16.0.6-gcc-11.2.1/lib/cmake/llvm" CACHE PATH "")
+set(FLEX_EXECUTABLE "/usr/tcetmp/packages/flex/flex-2.6.4/bin/flex" CACHE PATH "")
