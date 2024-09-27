a697668bb7bcca4ef990060d4d750f8340d79f86
Stephanie Brink <brink2@llnl.gov>
2024-09-27 16:46:32 -0700

    remove finding of adiak package

--- src/c/config/perfflowaspect_setup_deps.cmake
+++ src/c/config/perfflowaspect_setup_deps.cmake
@@ -7,22 +7,6 @@ endif()
 if(caliper_DIR)
     if(NOT PerfFlowAspect_FIND_QUIETLY)
         message(STATUS "PerfFlowAspect was built with Caliper Support")
-    endif()
-
-#    if(NOT ADIAK_DIR)
-#        set(ADIAK_DIR ${ASCENT_ADIAK_DIR})
-#    endif()
-#
-#    if(ADIAK_DIR)
-#        if(NOT PerfFlowAspect_FIND_QUIETLY)
-#            message(STATUS "Looking for Adiak at: ${ADIAK_DIR}/lib/cmake/adiak")
-#        endif()
-#        # find adiak first
-#        find_package(adiak REQUIRED
-#                     NO_DEFAULT_PATH
-#                     PATHS ${ADIAK_DIR}/lib/cmake/adiak)
-#    endif()
-    if(NOT PerfFlowAspect_FIND_QUIETLY)
         message(STATUS "Looking for Caliper at: ${caliper_DIR}/share/cmake/caliper")
     endif()
 
