ceb0ee810947c85373c67d483650033bc9a953f6
Stephanie Brink <brink2@llnl.gov>
2024-10-01 10:19:15 -0700

    update LLVM dir in same top location for clang compiler

--- src/c/host-configs/lassen-4.14.0-ppc64le-clang@16.0.6-cuda@11.8.0-gcc@11.2.1.cmake
+++ src/c/host-configs/lassen-4.14.0-ppc64le-clang@16.0.6-cuda@11.8.0-gcc@11.2.1.cmake
@@ -10,5 +10,5 @@
 
 set(CMAKE_C_COMPILER "/usr/tce/packages/clang/clang-16.0.6-cuda-11.8.0-gcc-11.2.1/bin/clang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-16.0.6-cuda-11.8.0-gcc-11.2.1/bin/clang++" CACHE PATH "")
-set(LLVM_DIR "/usr/tce/packages/clang/clang-ibm-16.0.6-cuda-11.8.0-gcc-11.2.1/lib/cmake/llvm" CACHE PATH "")
+set(LLVM_DIR "/usr/tce/packages/clang/clang-16.0.6-cuda-11.8.0-gcc-11.2.1/lib/cmake/llvm" CACHE PATH "")
 set(FLEX_EXECUTABLE "/usr/tcetmp/packages/flex/flex-2.6.4/bin/flex" CACHE PATH "")
