6a215ceffde3d184d13ce35c6fd742c121b9b273
Stephanie Brink <brink2@llnl.gov>
2024-10-01 09:48:23 -0700

    update/add lassen host configs for clang 16.0.6, cuda, gcc modules

---
+++ src/c/host-configs/lassen-4.14.0-ppc64le-clang@16.0.6-cuda@10.1.243-gcc@4.9.3.cmake
@@ -0,0 +1,14 @@
+##############################################################
+# Copyright 2021 Lawrence Livermore National Security, LLC
+# (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+#
+# This file is part of the Flux resource manager framework.
+# For details, see https://github.com/flux-framework.
+#
+# SPDX-License-Identifier: LGPL-3.0
+##############################################################
+
+set(CMAKE_C_COMPILER "/usr/tce/packages/clang/clang-16.0.6-cuda-10.1.243-gcc-4.9.3/bin/clang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-16.0.6-cuda-10.1.243-gcc-4.9.3/bin/clang++" CACHE PATH "")
+set(LLVM_DIR "/usr/tce/packages/clang/clang-ibm-16.0.6-cuda-10.1.243-gcc-4.9.3/lib/cmake/llvm" CACHE PATH "")
+set(FLEX_EXECUTABLE "/usr/tcetmp/packages/flex/flex-2.6.4/bin/flex" CACHE PATH "")
