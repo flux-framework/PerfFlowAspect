a6f116766445ee7ab4e0e36351ec74e75ccfca8b
Stephanie Brink <brink2@llnl.gov>
2025-04-04 11:52:57 -0700

    add host config for tuo clang18

---
+++ src/c/host-configs/tuolumne-4.18.0-x86_64-rocm@6.2.1-amdclang@18.0.0.cmake
@@ -0,0 +1,12 @@
+##############################################################
+# Copyright 2021 Lawrence Livermore National Security, LLC
+# (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+#
+# This file is part of the Flux resource manager framework.
+# For details, see https://github.com/flux-framework.
+#
+# SPDX-License-Identifier: LGPL-3.0
+##############################################################
+
+set(CMAKE_C_COMPILER "/opt/rocm-6.2.1/lib/llvm/bin/amdclang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/opt/rocm-6.2.1/lib/llvm/bin/amdclang++" CACHE PATH "")
