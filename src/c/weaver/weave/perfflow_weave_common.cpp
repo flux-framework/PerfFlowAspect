f3b404648a5fccf2b368e69a01f81c48a65161cc
Spencer Scott Greene <ssgreene@wpi.edu>
2025-04-11 04:02:59 -0700

    Insert adiak_init() into main functions
    Here we insert adiak_init every time the module pass encounters a main function. The CMakeLists should be cleaned up to properly integrate Adiak. Need to add in the adiak callback and validate this actually works.

--- src/c/weaver/weave/perfflow_weave_common.cpp
+++ src/c/weaver/weave/perfflow_weave_common.cpp
@@ -26,6 +26,16 @@ bool weave_ns::WeaveCommon::modifyAnnotatedFunctions(Module &m)
         return false;
     }
 
+    Function *main = m.getFunction("main");
+
+    if (main != NULL) {
+        outs() << "Inserting Adiak?\n";
+        insertAdiak(m, *main);
+    }
+    else {
+        outs () << "No main";
+    }
+
     bool changed = false;
 
     if (annotations->getNumOperands() <= 0)
@@ -193,6 +203,19 @@ bool weave_ns::WeaveCommon::insertBefore(Module &m, Function &f, StringRef &a,
     args.push_back(v4);
     args.push_back(v5);
     builder.CreateCall(before, args);
+
+    return true;
+}
+
+bool weave_ns::WeaveCommon::insertAdiak(Module &m, Function &f) {
+    LLVMContext &context = m.getContext();
+
+
+    FunctionCallee adiak_init = m.getOrInsertFunction("adiak_init", FunctionType::get(Type::getVoidTy(context), false));
+    BasicBlock &entry = f.getEntryBlock();
+    IRBuilder<> builder(&entry, entry.begin());
+
+    builder.CreateCall(adiak_init);
 
     return true;
 }
