91cfb5a422851f50846e2870294540fde1880c1f
Tapasya Patki <patki1@llnl.gov>
2025-05-19 14:22:32 -0700

    Remove Adiak from this build. To be added with Spencer's build

--- src/c/cmake/thirdparty/FindCaliper.cmake
+++ src/c/cmake/thirdparty/FindCaliper.cmake
@@ -1,19 +1,3 @@
-## most common case: caliper is built with adiak support
-## and caliper needs us to find adiak, or else find_pacakge caliper
-## will fail
-#
-## Check for ADIAK_DIR
-#
-#if(NOT ADIAK_DIR)
-#    MESSAGE(FATAL_ERROR "Caliper support needs explicit ADIAK_DIR")
-#endif()
-#
-#message(STATUS "Looking for Adiak in: ${ADIAK_DIR}")
-#
-#find_package(adiak REQUIRED
-#             NO_DEFAULT_PATH
-#             PATHS ${ADIAK_DIR}/lib/cmake/adiak)
-
 message(STATUS "Looking for Caliper in: ${caliper_DIR}")
 
 find_package(caliper REQUIRED
